38b96c1fcccfa3eac9ae63c5cc2e5211a6b665bd
valerii.chernikov <valerii.chernikov@excelfore.com>
2025-03-10 12:39:33 -0700

    ESYNC-8991 - avoid new connections for sequence DDL operations

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/NativeJDBCSeq.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/NativeJDBCSeq.java
@@ -320,9 +320,11 @@ public class NativeJDBCSeq
                     // as to not potentially insert records ahead of what the database thinks is the next sequence
                     // value.
 
-                    // first we have to allocate a new connection as some databases do an implicit commit
-                    // if a DDL gets changed. Others do blow up on a DDL change
-                    try (Connection newConn = getConnection(store, true)) {
+                    // We may have to allocate a new connection as some databases do an implicit commit
+                    // if a DDL gets changed and others do blow up on a DDL change, so ask the DBDictionary
+                    // whether to do so.
+
+                    try (Connection newConn = getConnection(store, dict.needNewDDLConn)) {
 
                         if (!dict.isSequenceIncrementCorrect(newConn, _seq)) {
 
